6693375d1cbfdaebdc8c895f14e3493d26fa5525
DanielPopOut <stephane.tchangang@gmail.com>
2018-02-18 15:04:49 -0500

    Reessai Premier commit

---
+++ app/app.ts
@@ -0,0 +1,118 @@
+/**
+ * app.ts - Configures an Express application.
+ *
+ * @authors Nicolas Richard, Emilio Riviera
+ * @date 2017/01/09
+ */
+
+import * as express from 'express';
+import * as path from 'path';
+import * as logger from 'morgan';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+
+import * as indexRoute from './routes/index';
+
+export class Application {
+
+    public app: express.Application;
+
+    /**
+     * Constructor.
+     *
+     * @class Server
+     * @constructor
+     */
+    constructor() {
+
+        // Application instantiation
+        this.app = express();
+
+        // configure this.application
+        this.config();
+
+        // configure routes
+        this.routes();
+    }
+
+    /**
+     * Bootstrap the application.
+     *
+     * @class Server
+     * @method bootstrap
+     * @static
+     * @return {ng.auto.IInjectorService} Returns the newly created injector for this this.app.
+     */
+    public static bootstrap(): Application {
+        return new Application();
+    }
+
+    /**
+     * The routes function.
+     *
+     * @class Server
+     * @method routes
+     */
+    public routes() {
+        let router: express.Router;
+        router = express.Router();
+
+        // create routes
+        const index: indexRoute.Index = new indexRoute.Index();
+
+        router.post('/description', index.ReceiveTask.bind(index.ReceiveTask));
+        // use router middleware
+        this.app.use(router);
+
+
+        this.app.get('/', function (req, res) {
+            res.sendFile(path.join(__dirname + '/angular_build/index.html'));
+        });
+
+
+        // Gestion des erreurs
+        this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+            const err = new Error('Not Found');
+            next(err);
+        });
+
+        // development error handler
+        // will print stacktrace
+        if (this.app.get('env') === 'development') {
+            this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+                res.status(err.status || 500);
+                res.send({
+                    message: err.message,
+                    error: err
+                });
+            });
+        }
+
+        // production error handler
+        // no stacktraces leaked to user (in production env only)
+        this.app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+            res.status(err.status || 500);
+            res.send({
+                message: err.message,
+                error: {}
+            });
+        });
+    }
+
+    /**
+     * The config function.
+     *
+     * @class Server
+     * @method config
+     */
+    private config() {
+        // Middlewares configuration
+        this.app.use(logger('dev'));
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({extended: true}));
+        this.app.use(cookieParser());
+        this.app.use(express.static(path.join(__dirname, '/angular_build')));
+        this.app.use(cors());
+    }
+}
